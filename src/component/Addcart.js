9c32c9b5d1819bceb08eeed992d2cc12b5f41578
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-11-07 18:20:36 +0530

    popups

--- src/component/Addcart.js
+++ src/component/Addcart.js
@@ -4,7 +4,7 @@ const Addtocart = () => {
     const[count,setCount]=useState(0)
   return (
     <div>
-        <button id='addcart' onClick={()=>setCount(count+1)}>Addtocart</button>
+        <button id='#addtocart' onClick={()=>setCount(count+1)}>Addtocart</button>
        {" "} <span id='cartcount'>[{count}]</span>
     </div>
   )
