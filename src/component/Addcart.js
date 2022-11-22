188da2c28f71085831d037f105a9291d0c86839e
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2022-11-22 17:27:44 +0530

    add to cart event listener added

--- src/component/Addcart.js
+++ src/component/Addcart.js
@@ -1,16 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Addtocart = () => {
     const[count,setCount]=useState(0)
-    
-    const handelsubmit=()=>{
-        setCount(count+1)
-        window.OptiMonkOnReady(count)
+    useEffect(()=>{
+      let d = document.querySelectorAll("#addtocart")
+     console.log(d);
+     console.log( d[0].innerHTML)
+   d.forEach((r)=>{
+     r.addEventListener('click',()=>{
+       window.OptiMonkOnReady(count)
+       })
+   })
+   },[])
+
+    const update = ()=>{
+
+      console.log("nope");
+      setCount(count+1)
     }
   return (
     <div>
-        <button id='addtocart' onClick={()=>handelsubmit()}>Addtocart</button>
-      <span id='_cart_total'>[{count}]</span>
+        <button id='addtocart' onClick={()=>update()}>Addtocart</button>
+       <span id='_cart_total'>[{count}]</span>
+       <button id='addtocart' onClick={()=>update()}>Addtocart</button>
+       <span id='_cart_total'>[{count}]</span>
     </div>
   )
 }
