d38fdd3a43ca82e238b70de346b14c1801fe5f91
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2022-11-22 13:10:07 +0530

    checking window element for optimonk

--- src/component/Addcart.js
+++ src/component/Addcart.js
@@ -3,14 +3,9 @@ import React, { useState } from 'react'
 const Addtocart = () => {
     const[count,setCount]=useState(0)
     
-    var OptiMonkOnReady= function(){
-        var cart=window.OptiMonk.Visitor.createAdapter();
-        cart.Cart.add(`product${count}`,{'quantity':count,'price':count+100})
-        console.log(cart.Cart.get())
-    }
     const handelsubmit=()=>{
         setCount(count+1)
-        OptiMonkOnReady()
+        window.OptiMonkOnReady(count)
     }
   return (
     <div>
