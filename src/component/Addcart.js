605ed83955840c5c77a60428c32ed3e6fe6530ac
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2022-11-22 12:33:47 +0530

    cart optimonk adapter added

--- src/component/Addcart.js
+++ src/component/Addcart.js
@@ -2,10 +2,16 @@ import React, { useState } from 'react'
 
 const Addtocart = () => {
     const[count,setCount]=useState(0)
+    
+    const handelsubmit=()=>{
+        setCount(count+1)
+        var cart=window.OptiMonk.Visitor.createAdapter();
+        cart.Cart.add(`product${count}`,{'quantity':count,'price':count+100})
+    }
   return (
     <div>
-        <button id='addtocart' onClick={()=>setCount(count+1)}>Addtocart</button>
-       {" "} <span id='_cart_total'>[{count}]</span>
+        <button id='addtocart' onClick={()=>handelsubmit()}>Addtocart</button>
+      <span id='_cart_total'>[{count}]</span>
     </div>
   )
 }
